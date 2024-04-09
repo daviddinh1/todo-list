e3ac809ef3a329e2a90be972efb6a9d34d712521
daviddinh1 <91696365+daviddinh1@users.noreply.github.com>
2024-04-09 18:31:12 -0500

    created user storage tool using local storage that holds and outputs data of new dynamically created todos on projects

--- src/index.js
+++ src/index.js
@@ -11,8 +11,12 @@ class items{
 }
 
 let example = new items("code","code react","1-20-2020","high");
+let example2 = new items("code2","code react2","1-20-2020","high");
+
 
 //later on figure out on how to store the todos
+let projectHolder = {}
+let todoHolder = []
 
 function outputTodos(items,index){ //does not createTodos but outputs it to our html file
  const inboxContainer = document.getElementsByClassName("inbox-container");
@@ -42,10 +46,10 @@ function outputTodos(items,index){ //does not createTodos but outputs it to our 
  todoContainer.appendChild(todoItem2);
  todoContainer.appendChild(todoItem3);
  todoContainer.appendChild(deleteBtn);
- console.log(index);
  inboxContainer[index].appendChild(todoContainer);
  todoChecker(checkerBtn,todoItem1,todoItem2,todoItem3);
  deleteTodo(deleteBtn,todoContainer);
+
 }
 
 function createTodos(){ //uses dialog we made to create todos
@@ -56,12 +60,6 @@ function createTodos(){ //uses dialog we made to create todos
  showCreateTodo.addEventListener("click",()=>{
   dialogTodo.showModal();
  });
-
- let valueTitle = dialogTodo.querySelector("#title").value;
- let valueDesc = dialogTodo.querySelector("#description").value;
- let valueDuedate = dialogTodo.querySelector("#dueDate").value;
- let valuePrio = dialogTodo.querySelector("#priority").value;
-
  createBtn.addEventListener("click", (event) => {
   event.preventDefault(); // We don't want to submit this fake form
   let valueTitle = dialogTodo.querySelector("#title").value;
@@ -69,7 +67,14 @@ function createTodos(){ //uses dialog we made to create todos
   let valueDuedate = dialogTodo.querySelector("#dueDate").value;
   let valuePrio = dialogTodo.querySelector("#priority").value;
   let newTodo = new items(valueTitle,valueDesc,valueDuedate,valuePrio);
+  todoHolder.push(newTodo);
+  console.log(todoHolder);
+  projectHolder[i] = todoHolder; //holds the array for storage
+  console.log(projectHolder);
+
   outputTodos(newTodo,index); //how to get the correct index to output this on a page
+  populateStorage();
+
  });
 }
 
@@ -79,7 +84,6 @@ function todoChecker(checkerBtn,todoItem1,todoItem2,todoItem3){
    todoItem1.style.textDecoration = "line-through";
    todoItem2.style.textDecoration = "line-through";
    todoItem3.style.textDecoration = "line-through";
-   console.log("its working");
   }
   else{
     todoItem1.style.textDecoration = "";
@@ -96,7 +100,6 @@ function deleteTodo(deleteBtn,todoContainer){
 }
 
 let i = 0;
-
 function createProject(){
  const projectsContainer = document.querySelector(".projects");
  const btnProjects = document.querySelector("#btnProjects");
@@ -114,16 +117,16 @@ function createProject(){
   project(i); //creates todo page
   event.preventDefault(); 
   let projectTitle = showProjectsDialog.querySelector("#projectTitle").value;
-  console.log(projectTitle);
   let projectDiv = document.createElement("button");
   projectDiv.textContent = projectTitle;
   projectDiv.setAttribute("class","projectDiv");
   projectsContainer.append(projectDiv);
- 
+
  });
 }
 
 let index = 0;
+
 function switchProject() {
   const container = document.querySelector('.container'); // Parent container where projectDivs will be added
 
@@ -147,12 +150,63 @@ function switchProject() {
   });
 }
 
+//first step figure out how to save the dynamically created items in the object/arr
+
+//create function that saves data to local storage
+function populateStorage(){
+  const serializedData = JSON.stringify(projectHolder); //changes object into string
+  localStorage.setItem('userData', serializedData); //stores it in storage
+  console.log(serializedData);
+}
+
+//create function that finds data from local storage
+function getStorage(){
+  const storedData = localStorage.getItem('userData');
+  const userData = JSON.parse(storedData);
+  let valTitle;
+  let valDD;
+  let valDesc;
+  let prio;
+  i++;
+
+  project(i);
+  const projectsContainer = document.querySelector(".projects");
+
+  let projectTitle = "stored project";
+  //console.log(projectTitle);
+  let projectDiv = document.createElement("button");
+  projectDiv.textContent = projectTitle;
+  projectDiv.setAttribute("class","projectDiv");
+  projectsContainer.append(projectDiv);
+
+  for(const key in userData){
+    console.log("key: ",key);
+    const value = userData[key];
+    for(let j = 0 ; j < value.length;j++){
+      valTitle = value[j].title;
+      valDesc = value[j].description;
+      valDD = value[j].dueDate;
+      prio = value[j].priority;
+      revampUI(i,valTitle,valDesc,valDD,prio);
+    }
+  }
 
 
+}
 
+function revampUI(key,title,description,dueDate,priority){
+  //todos
+  let valueTitle = title;
+  let valueDesc = description;
+  let valueDuedate = dueDate;
+  let valuePrio = priority;
+  let newTodo = new items(valueTitle,valueDesc,valueDuedate,valuePrio);
+  outputTodos(newTodo,key);
+}
 
 outputTodos(example,index);
+outputTodos(example2,index);
 createTodos();
 createProject();
 switchProject();
-
+getStorage();
